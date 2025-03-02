a1d01a825739854babe8f05f1dc91e1cf16e9b5e
Jamezs Gladney <panthrowzay@gmail.com>
2025-03-02 18:20:45 -0500

    Update IronMarioTracker.js
    Remove mixed in Electron Code

--- IronMarioTracker.js
+++ IronMarioTracker.js
@@ -1,10 +1,5 @@
 // Project64 JavaScript Tracker Script (Mirrors IronMarioTracker.lua)
 
-const net = require('net');
-const { ipcRenderer } = require('electron');
-const { app, BrowserWindow, ipcMain } = require('electron');
-let mainWindow;
-
 // Memory address configuration
 const CONFIG = {
     MEM: {
@@ -71,40 +66,4 @@ server.on('connection', function(connection) {
         connection.write(JSON.stringify(state));
     }, 1000);
     connection.on('close', () => { clearInterval(outputer); });
-});
-
-// Electron client to receive data and render overlay
-const client = net.createConnection({ port: 1337 }, () => {
-    console.log('Connected to server');
-});
-
-client.on('data', (data) => {
-    ipcRenderer.send('gameStateUpdate', JSON.parse(data));
-});
-
-client.on('end', () => {
-    console.log('Disconnected from server');
-});
-
-// Create Electron window
-app.whenReady().then(() => {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        transparent: true,
-        frame: false,
-        alwaysOnTop: true,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        }
-    });
-    mainWindow.loadURL(`file://${__dirname}/overlay.html`);
-});
-
-// Handle game state updates in renderer process
-ipcMain.on('gameStateUpdate', (event, newState) => {
-    if (mainWindow) {
-        mainWindow.webContents.send('updateOverlay', newState);
-    }
 });
