2d70781056789c69a784e3936490e7e70a2c6444
mysticentity <36752387+mysticentity@users.noreply.github.com>
2025-03-02 09:24:34 -0800

    Update MemReader.js

--- MemReader.js
+++ MemReader.js
@@ -17,9 +17,25 @@ const processName = "Project64.exe";
 
 // Memory addresses
 const MEM_ADDRESSES = {
-  STARS: 0x1A0334,
-  LEVEL_ID: 0x18FD78,
-  MARIO_POS: 0x1A037C
+   MARIO_BASE: 0x1a0340,
+        HUD_BASE: 0x1a0330,
+        CURRENT_LEVEL_ID: 0x18fd78,
+        CURRENT_SEED: 0x1cdf80,
+        DELAYED_WARP_OP: 0x1a031c,
+        INTENDED_LEVEL_ID: 0x19f0cc,
+        CURRENT_SONG_ID: 0x19485e
+  
+};
+const CONFIG.MEM.MARIO = {
+    INPUT: CONFIG.MEM.MARIO_BASE + 0x2,
+    ACTION: CONFIG.MEM.MARIO_BASE + 0xC,
+    POS: CONFIG.MEM.MARIO_BASE + 0x3C,
+    HURT_COUNTER: CONFIG.MEM.MARIO_BASE + 0xB2
+};
+
+const CONFIG.MEM.HUD = {
+    STARS: CONFIG.MEM.HUD_BASE + 0x4,
+    HEALTH: CONFIG.MEM.HUD_BASE + 0x6
 };
 
 const float = ref.types.float;
@@ -57,12 +73,12 @@ const readGameState = () => {
   if (!processHandle) return null;
 
   const gameState = {
-    stars: readMemory(processHandle, MEM_ADDRESSES.STARS, int32),
-    levelId: readMemory(processHandle, MEM_ADDRESSES.LEVEL_ID, int32),
+    stars: readMemory(processHandle, CONFIG.MEM.HUD_BASE + 0x4, int32),
+    levelId: readMemory(processHandle, MEM_ADDRESSES.CURRENT_LEVEL_ID, int32),
     marioPos: {
-      x: readMemory(processHandle, MEM_ADDRESSES.MARIO_POS, float),
-      y: readMemory(processHandle, MEM_ADDRESSES.MARIO_POS + 4, float),
-      z: readMemory(processHandle, MEM_ADDRESSES.MARIO_POS + 8, float)
+      x: readMemory(processHandle, CONFIG.MEM.MARIO_BASE + 0x3C, float),
+      y: readMemory(processHandle, CONFIG.MEM.MARIO_BASE + 0x3C + 4, float),
+      z: readMemory(processHandle, CONFIG.MEM.MARIO_BASE + 0x3C + 8, float)
     }
   };
 
